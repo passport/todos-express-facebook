a57f7e9e4e64740c5207d691c5cfb4d62df9381a
Jared Hanson <jaredhanson@gmail.com>
2021-05-05 10:49:01 -0700

    Use middleware as generated by express-generator.

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 require('dotenv').config();
 
-var path = require('path');
 var express = require('express');
 var passport = require('passport');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 var authRouter = require('./routes/auth');
 
-
-// Create a new Express application.
 var app = express();
 
 require('./boot/auth')();
 
-// Configure view engine to render EJS templates.
-app.set('views', __dirname + '/views');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
-app.use(require('morgan')('combined'));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
+// session setup
+//
+// This sequence of middleware is necessary for login sessions.  The first
+// middleware loads session data and makes it available at `req.session`.  The
+// next lines initialize Passport and authenticate the request based on session
+// data.  If session data contains an authenticated user, the user is set at
+// `req.user`.
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
