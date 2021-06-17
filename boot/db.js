2b1ac1e559a66f82c1da57a6a6e85a315f8bcaa5
Jared Hanson <jaredhanson@gmail.com>
2021-06-17 16:58:05 -0700

    Provision users.

--- boot/db.js
+++ boot/db.js
@@ -5,7 +5,12 @@ module.exports = function() {
 
   db.serialize(function() {
     db.run("CREATE TABLE IF NOT EXISTS users (username TEXT, hashed_password BLOB, salt BLOB, name TEXT)");
-    db.run("CREATE TABLE IF NOT EXISTS federated_credentials (provider TEXT, subject TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS federated_credentials ( \
+      provider TEXT NOT NULL, \
+      subject TEXT NOT NULL, \
+      user_id INTEGER NOT NULL, \
+      PRIMARY KEY (provider, subject) \
+    )");
 
   });
 
--- routes/auth.js
+++ routes/auth.js
@@ -11,18 +11,53 @@ router.get('/login', function(req, res, next) {
 router.get('/login/facebook', passport.authenticate('facebook'));
 
 router.get('/return', 
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  passport.authenticate('facebook', { assignProperty: 'federatedUser', failureRedirect: '/login' }),
   function(req, res, next) {
-    db.run('INSERT INTO federated_credentials (provider, subject) VALUES (?, ?)', [
+    db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
       'https://www.facebook.com',
-      req.user.id
-    ], function(err) {
+      req.federatedUser.id
+    ], function(err, row) {
       if (err) { return next(err); }
-      next();
+      if (!row) {
+        db.run('INSERT INTO users (name) VALUES (?)', [
+          req.federatedUser.displayName
+        ], function(err) {
+          if (err) { return next(err); }
+          
+          var id = this.lastID;
+          db.run('INSERT INTO federated_credentials (provider, subject, user_id) VALUES (?, ?, ?)', [
+            'https://www.facebook.com',
+            req.federatedUser.id,
+            id
+          ], function(err) {
+            if (err) { return next(err); }
+            var user = {
+              id: id.toString(),
+              displayName: req.federatedUser.displayName
+            };
+            req.login(user, function(err) {
+              if (err) { return next(err); }
+              res.redirect('/');
+            });
+          });
+        });
+      } else {
+        db.get('SELECT rowid AS id, username, name FROM users WHERE rowid = ?', [ row.user_id ], function(err, row) {
+          if (err) { return next(err); }
+    
+          // TODO: Handle undefined row.
+          var user = {
+            id: row.id.toString(),
+            username: row.username,
+            displayName: row.name
+          };
+          req.login(user, function(err) {
+            if (err) { return next(err); }
+            res.redirect('/');
+          });
+        });
+      }
     });
-  },
-  function(req, res) {
-    res.redirect('/');
   });
 
 router.get('/logout', function(req, res, next) {
