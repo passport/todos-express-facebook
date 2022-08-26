ca0b5882bb0cd587099c57ad6a4c0a00109e3106
Jared Hanson <jaredhanson@gmail.com>
2022-08-26 12:00:56 -0700

    Update app.

--- app.js
+++ app.js
@@ -1,42 +1,71 @@
 require('dotenv').config();
 
+var createError = require('http-errors');
 var express = require('express');
-var passport = require('passport');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var csrf = require('csurf');
+var passport = require('passport');
 var logger = require('morgan');
+
+// pass the session to the connect sqlite3 module
+// allowing it to inherit from session.Store
+var SQLiteStore = require('connect-sqlite3')(session);
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
-var myaccountRouter = require('./routes/myaccount');
 
 var app = express();
-
-require('./boot/db')();
-require('./boot/auth')();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.locals.pluralize = require('pluralize');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// session setup
-//
-// This sequence of middleware is necessary for login sessions.  The first
-// middleware loads session data and makes it available at `req.session`.  The
-// next lines initialize Passport and authenticate the request based on session
-// data.  If session data contains a logged in user, the user is set at
-// `req.user`.
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+}));
+app.use(csrf());
+app.use(passport.authenticate('session'));
+app.use(function(req, res, next) {
+  var msgs = req.session.messages || [];
+  res.locals.messages = msgs;
+  res.locals.hasMessages = !! msgs.length;
+  req.session.messages = [];
+  next();
+});
+app.use(function(req, res, next) {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/', authRouter);
-app.use('/myaccount', myaccountRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
