59af987d55c9f50c43ce0b6591e87ae3717019a5
Jared Hanson <jaredhanson@gmail.com>
2022-08-26 11:57:14 -0700

    Update db.

--- db.js
+++ db.js
@@ -1,3 +1,33 @@
 var sqlite3 = require('sqlite3');
+var mkdirp = require('mkdirp');
 
-module.exports = new sqlite3.Database('db.sqlite3');
+mkdirp.sync('./var/db');
+
+var db = new sqlite3.Database('./var/db/todos.db');
+
+db.serialize(function() {
+  db.run("CREATE TABLE IF NOT EXISTS users ( \
+    id INTEGER PRIMARY KEY, \
+    username TEXT UNIQUE, \
+    hashed_password BLOB, \
+    salt BLOB, \
+    name TEXT \
+  )");
+  
+  db.run("CREATE TABLE IF NOT EXISTS federated_credentials ( \
+    id INTEGER PRIMARY KEY, \
+    user_id INTEGER NOT NULL, \
+    provider TEXT NOT NULL, \
+    subject TEXT NOT NULL, \
+    UNIQUE (provider, subject) \
+  )");
+  
+  db.run("CREATE TABLE IF NOT EXISTS todos ( \
+    id INTEGER PRIMARY KEY, \
+    owner_id INTEGER NOT NULL, \
+    title TEXT NOT NULL, \
+    completed INTEGER \
+  )");
+});
+
+module.exports = db;
